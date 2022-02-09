9f178c2cadb107b09727e90645506b6bed5c160e
Hillary Murefu <hillarywang2005@gmail.com>
2022-02-09 14:23:42 +0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -5,6 +5,12 @@ import numpy as np
 
 
 def load_data(messages_filepath, categories_filepath):
+    '''INNPUT
+    messages_filepath; path with messages
+    categories_filepath: path with the categories
+    
+    OUTPUT
+    df: a dataframe with messages and labels (categories)'''
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = pd.merge(messages,categories,how='left',on='id')
@@ -12,6 +18,12 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+        '''INNPUT
+    df: a dataframe with messages and labels (categories)
+    
+    OUTPUT
+    df: cleaned dataframe
+    '''
     categories = df['categories'].str.split(';',expand=True)
     row = categories.iloc[0]
     row2=row.apply(lambda x: x[:-2])
@@ -33,11 +45,14 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    '''writes dataframe to db
+    INPUT: a dataframe and a db filename'''
     engine = create_engine('sqlite:///'+database_filename)
     df.to_sql('table1', engine, index=False) 
 
 
 def main():
+    '''main function to call the others'''
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
